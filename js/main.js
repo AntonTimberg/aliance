b9b1583bb2469af09b31b7bdab0ab7035416c159
AntonTimberg <antonoffice2015@gmail.com>
2025-01-26 02:21:15 +0400

    add: adapt menu

--- js/main.js
+++ js/main.js
@@ -4,18 +4,32 @@ const mMenuToggle = document.querySelector('.mobile-menu-toggle');
 const menu = document.querySelector('.mobile-menu');
 
 window.addEventListener('scroll', () => {
-    if (this.scrollY > 1) {
+    if (menu.classList.contains('is-open')) {
+        return;
+    }
+
+    if (window.scrollY > 1) {
         navbar.classList.add("navbar-light");
         logo.href.baseVal = "img/sprites.svg#logo";
-
-        
     } else {
         navbar.classList.remove("navbar-light");
         logo.href.baseVal = "img/sprites.svg#logo-light";
     }
 }); 
 
-mMenuToggle.addEventListener('click', () => {
+mMenuToggle.addEventListener('click', (event) => {
     event.preventDefault();
     menu.classList.toggle("is-open");
+
+    mMenuToggle.classList.toggle("menu-opened");
+
+    if (menu.classList.contains('is-open')) {
+        logo.href.baseVal = "img/sprites.svg#logo";
+    } else {
+        if (window.scrollY > 1) {
+            logo.href.baseVal = "img/sprites.svg#logo";
+        } else {
+            logo.href.baseVal = "img/sprites.svg#logo-light";
+        }
+    }
 });
