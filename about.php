9475ec3ea4e4e25987d669e0b4464e643a23be90
AntonTimberg <antonoffice2015@gmail.com>
2025-02-26 16:32:15 +0400

    fix: navbar php

--- about.php
+++ about.php
@@ -92,51 +92,7 @@
     </div>
     <!-- mobile-menu -->
 
-    <nav class="navbar">
-        <a href="#" class="mobile-menu-toggle">
-            <div class="mobile-menu-line"></div>
-            <div class="mobile-menu-line"></div>
-            <div class="mobile-menu-line"></div>
-        </a>
-        <a href="./" class="header-logo">
-            <svg class="logo-svg">
-                <use href="img/sprites.svg#logo"></use>
-            </svg>
-        </a>
-        <ul class="header-nav">
-            <li class="header-nav-item">
-                <a href="#" class="header-nav-link">О компании</a>
-            </li>
-            <li class="header-nav-item">
-                <a href="#" class="header-nav-link">Контрактное производство</a>
-            </li>
-            <li class="header-nav-item">
-                <a href="#" class="header-nav-link">Собственные торговые марки</a>
-            </li>
-            <li class="header-nav-item">
-                <a href="#" class="header-nav-link">Новости</a>
-            </li>
-            <li class="header-nav-item">
-                <a href="#" class="header-nav-link">Контакты</a>
-            </li>
-        </ul>
-        <div class="header-phone">
-            <svg class="phone-icon" width="24" height="24">
-                <use href="img/sprites.svg#phone"></use>
-            </svg>
-            <a href="tel:+79999999999" class="header-phone-link">+7 (999) 999-99-99</a>
-        </div>
-        <!-- /.header-phone -->
-        <button class="navbar-button button">
-            <span class="button-icon">
-                <svg width="24" height="24" fill="#fff">
-                    <use href="img/sprites.svg#phone-white"></use>
-                </svg>
-            </span>
-            <span class="button-text">Получить консультацию</span>
-        </button>
-    </nav>
-    <!-- /.navbar -->
+    <?php include_once('navbar.php'); ?>
 
     <container class="header-title">
         <div class="header-title-content">
